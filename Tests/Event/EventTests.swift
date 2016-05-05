9f4a59f7e7262d8905b2fd56fe0303529bf2422b
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2016-05-05 14:18:01 +0300

    Project setup for all Apple platforms Linux Package.swift

--- Tests/Event/EventTests.swift
+++ Tests/Event/EventTests.swift
@@ -61,16 +61,6 @@ class EventEmitterTest : EventEmitterProtocol {
 
 class EventTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
         let ec = ExecutionContext(kind: .Parallel)
         
@@ -114,13 +104,6 @@ class EventTests: XCTestCase {
         
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
